51bcf87de99413f45da88a9d628e64cd2ccd21cd
unknown <maria.rodriguez@northwestern.edu>
2023-03-01 18:21:46 -0600

    setting up start page

--- server.js
+++ server.js
@@ -1,6 +1,90 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 const sequelize = require('./config/connection');
 
+//start page with options
+function startPage() {
+    inquirer   
+        .prompt({  
+            type:"list",
+            name: "yourPick",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "View All Roles",
+                "View All Departments",
+                "Add Department",
+                "Update Employee Managers", //Extra
+                "View Employees By Manager", //Extra
+                "View Employees By Department", //Extra
+                "Delete Departments", //Extra
+                "Delete Roles", //Extra
+                "Delete Employees", //Extra
+                "EXIT"
+                ]
+            })
+        .then(function(answer) {
+            switch (answer.yourPick) {
+                case "View All Employees":
+                  viewEmployees();
+                  break;
+
+                case "Add Employee":
+                  AddEmployee();
+                  break;
+                
+                case "Update Employee Role":
+                  UpdateEmployeeRole();
+                  break;
+
+                case "View All Roles":
+                  viewRoles();
+                  break;
+
+                case "View All Departments":
+                  viewDepartments();
+                  break;
+
+                case "Add Department":
+                  addDepartment();
+                  break;
+
+                case "Update Employee Managers":
+                  updateEmployeeManagers();
+                  break;
+
+                case "View Employees By Manager":
+                  viewEmployeesByManagers();
+                  break;
+
+                case "View Employees By Department":
+                  viewEmployeesByDept();
+                  break;
+
+                case "Delete Departments":
+                  deleteDepartments();
+                  break;
+
+                case "Delete Roles":
+                  deleteRoles();
+                  break;
+
+                case "Delete Employees":
+                  deleteEmployees();
+                  break;
+
+                case "EXIT":
+                  console.log("Thnaks for using Employee Tracker! See you Later!")
+                  EXIT();
+                  
+            }
+        
+        });
+    
+    }
 
 //Presented with options: View all departments, view all roles, view all employees, add a department, add a role, add an employee. and update an employee role
 //Department: Formated Table showing department names and department IDs
