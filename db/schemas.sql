4ac64117090338222de2e33b1733348fd69c8382
unknown <maria.rodriguez@northwestern.edu>
2023-05-23 00:43:18 -0500

    syntax issues

--- db/schemas.sql
+++ db/schemas.sql
@@ -3,21 +3,21 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(11,2) NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -26,5 +26,5 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    REFERENCES employees(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
-INSERT INTO department (name)
+INSERT INTO departments (department_name)
 VALUES ("Finance"),
        ("Marketing"),
        ("IT"),
        ("Engineering");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Market Analyst", "63000", 2),
-       ("IT Support Specialist", "49000", 3),
-       ("Marketing Manager", "94000", 2),
-       ("Portfolio Manager", "115000", 1),
-       ("Senior Analyast", "88000", 1)
-       ("Software Engineer", "105000", 4),
-       ("Engineering Manager", "105000", 4),
-       ("IT Manager", "107000", 3);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Market Analyst", 63000, 2),
+       ("IT Support Specialist", 49000, 3),
+       ("Marketing Manager", 94000, 2),
+       ("Portfolio Manager", 115000, 1),
+       ("Senior Analyast", 88000, 1)
+       ("Software Engineer", 105000, 4),
+       ("Engineering Manager", 105000, 4),
+       ("IT Manager", 107000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("James", "Robert", 5, 4),
        ("Jennifer", "Smith", 3, NULL),
        ("Mark", "Andrews", 6, 7),
